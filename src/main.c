20441705667f2f3e772cd12ab837c0c40960a053
Hannes Frank <hannes.frank@online.de>
2016-11-06 18:29:57 +0100

    Documented the little code, I already have (the main function).

--- src/main.c
+++ src/main.c
@@ -1,4 +1,20 @@
-int main()
+/**
+ * @file main.c
+ *
+ * File that contains the main function.
+ *
+ * @see main
+ */
+
+/**
+ * The main entry point of the program.
+ * This is the first function that is called, when the program starts.
+ *
+ * @param argc Number of command line arguments.
+ * @param argv List of all command line arguments.
+ * @return A status code (0 means success, anything else means something went wrong).
+ */
+int main(int argc, char **argv)
 {
 	return 0;
 }
