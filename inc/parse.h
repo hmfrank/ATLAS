c1051ead5660cfe3ad6a555d39d55eee76696182
Hannes Frank <hannes.frank@online.de>
2016-11-07 18:02:26 +0100

    Added parse.c and parse.h.

---
+++ inc/parse.h
@@ -0,0 +1,25 @@
+#ifndef ATLAS_PARSE_H
+#define ATLAS_PARSE_H
+
+#include "LogEntry.h"
+#include "PreReader.h"
+
+/**
+ * @file parse.h
+ *
+ * Contains function prototypes for parsing a log file.
+ */
+
+/**
+ * Parses a line of a log file to create a LogEntry.
+ *
+ * @param reader The reader to read from (must be at a line start in the log file).
+ * @param result A pointer to where the result is gonna be stored.
+ * @return Status code with the following meaning:<br>
+ * 0 = success<br>
+ * -1 = invalid parameter (e.g. `NULL`)<br>
+ * 1 = parsing error (wrong format or `EOF` reached)
+ */
+int parseLogEntry(struct PreReader *reader, struct LogEntry *result);
+
+#endif //ATLAS_PARSE_H
