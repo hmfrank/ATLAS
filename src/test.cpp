61665d625062240e1722361bb3b627ea6816c15a
Hannes Frank <hannes.frank@online.de>
2016-11-07 10:17:52 +0100

    Added unit test for prNext().

--- src/test.cpp
+++ src/test.cpp
@@ -6,8 +6,6 @@ extern "C"
 #include "../inc/PreReader.h"
 };
 
-// TODO: test prNext()
-
 TEST_CASE("PreReader initialization", "[prInit]")
 {
 	// some random file
@@ -19,4 +17,54 @@ TEST_CASE("PreReader initialization", "[prInit]")
 	REQUIRE(pr.file == file);
 	REQUIRE(pr.read_pos == 0);
 	REQUIRE(pr.write_pos == 0);
+}
+
+TEST_CASE("PreReader get next char", "[prNext]")
+{
+	struct PreReader pr;
+	char data[] = "abcdef\0\n\t\b!@#$";
+	FILE *file = fmemopen(data, 14, "r");
+
+	REQUIRE(file != NULL);
+	prInit(&pr, file);
+
+	// test normal reading
+	for (int i = 0; i < 14; i++)
+		REQUIRE(prNext(&pr) == (int)(data)[i]);
+
+	REQUIRE(prNext(&pr) == EOF);
+	REQUIRE(prNext(&pr) == EOF);
+
+	// test invalid parameter
+	REQUIRE(prNext(NULL) == EOF);
+
+	fseek(pr.file, 0, SEEK_SET);
+	pr.write_pos = 0;
+	pr.read_pos = 1;
+	REQUIRE(prNext(&pr) == EOF);
+
+	pr.write_pos = PREREADER_BUFFER_SIZE;
+	pr.read_pos = 6;
+	REQUIRE(prNext(&pr) == 0);
+
+	pr.write_pos = PREREADER_BUFFER_SIZE + 1;
+	REQUIRE(prNext(&pr) == EOF);
+
+	// close file
+	REQUIRE(fclose(file) == 0);
+
+	// test reload
+	char bigdata[PREREADER_BUFFER_SIZE + 1];
+	bigdata[PREREADER_BUFFER_SIZE - 1] = 'A';
+	bigdata[PREREADER_BUFFER_SIZE] = 'B';
+	file = fmemopen(bigdata, PREREADER_BUFFER_SIZE + 1, "r");
+
+	REQUIRE(file != NULL);
+	prInit(&pr, file);
+
+	for (int i = 0; i < PREREADER_BUFFER_SIZE + 1; i++)
+		REQUIRE(prNext(&pr) == (int)bigdata[i]);
+
+	REQUIRE(prNext(&pr) == EOF);
+	REQUIRE(prNext(&pr) == EOF);
 }
