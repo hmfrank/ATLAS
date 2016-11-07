eb8015ec93d8048a0eab87faa09806bad7dae612
Hannes Frank <hannes.frank@online.de>
2016-11-07 09:46:23 +0100

    Added unit test for prInit().

--- src/test.cpp
+++ src/test.cpp
@@ -1,5 +1,22 @@
 #define CATCH_CONFIG_MAIN
 #include "../inc/catch.hpp"
 
-// TODO: test prInit()
+extern "C"
+{
+#include "../inc/PreReader.h"
+};
+
 // TODO: test prNext()
+
+TEST_CASE("PreReader initialization", "[prInit]")
+{
+	// some random file
+	FILE *file = stdin;
+	struct PreReader pr;
+
+	prInit(&pr, file);
+
+	REQUIRE(pr.file == file);
+	REQUIRE(pr.read_pos == 0);
+	REQUIRE(pr.write_pos == 0);
+}
