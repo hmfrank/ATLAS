42ecf02684e0ea3ac5aaac0acd279969ae6cf0ab
Hannes Frank <hannes.frank@online.de>
2016-11-13 19:42:39 +0100

    Deleted PreReader.
    PreReader wasn't needed anymore.

--- src/test.cpp
+++ src/test.cpp
@@ -7,7 +7,6 @@ extern "C"
 #include "../inc/LogEntry.h"
 #include "../inc/macros.h"
 #include "../inc/parse.h"
-#include "../inc/PreReader.h"
 };
 
 /*
@@ -86,67 +85,4 @@ TEST_CASE("parse log entry", "[src/parse.c/parseLogEntry]")
 	REQUIRE(parseLogEntry(file, &entry) == 1);
 
 	fclose(file);
-}
-
-TEST_CASE("PreReader initialization", "[src/PreReader.c/prInit]")
-{
-	// some random file
-	FILE *file = stdin;
-	struct PreReader pr;
-
-	prInit(&pr, file);
-
-	REQUIRE(pr.file == file);
-	REQUIRE(pr.read_pos == 0);
-	REQUIRE(pr.write_pos == 0);
-}
-
-TEST_CASE("PreReader get next char", "[src/PreReader.c/prNext]")
-{
-	struct PreReader pr;
-	const char *data = "abcdef\0\n\t\b!@#$";
-	FILE *file = fmemopen((void*)data, 14, "r");
-
-	REQUIRE(file != NULL);
-	prInit(&pr, file);
-
-	// test normal reading
-	for (int i = 0; i < 14; i++)
-		REQUIRE(prNext(&pr) == (int)(data)[i]);
-
-	REQUIRE(prNext(&pr) == EOF);
-	REQUIRE(prNext(&pr) == EOF);
-
-	// test invalid parameter
-	REQUIRE(prNext(NULL) == EOF);
-
-	fseek(pr.file, 0, SEEK_SET);
-	pr.write_pos = 0;
-	pr.read_pos = 1;
-	REQUIRE(prNext(&pr) == EOF);
-
-	pr.write_pos = PREREADER_BUFFER_SIZE;
-	pr.read_pos = 6;
-	REQUIRE(prNext(&pr) == 0);
-
-	pr.write_pos = PREREADER_BUFFER_SIZE + 1;
-	REQUIRE(prNext(&pr) == EOF);
-
-	// close file
-	REQUIRE(fclose(file) == 0);
-
-	// test reload
-	char bigdata[PREREADER_BUFFER_SIZE + 1];
-	bigdata[PREREADER_BUFFER_SIZE - 1] = 'A';
-	bigdata[PREREADER_BUFFER_SIZE] = 'B';
-	file = fmemopen(bigdata, PREREADER_BUFFER_SIZE + 1, "r");
-
-	REQUIRE(file != NULL);
-	prInit(&pr, file);
-
-	for (int i = 0; i < PREREADER_BUFFER_SIZE + 1; i++)
-		REQUIRE(prNext(&pr) == (int)bigdata[i]);
-
-	REQUIRE(prNext(&pr) == EOF);
-	REQUIRE(prNext(&pr) == EOF);
 }
