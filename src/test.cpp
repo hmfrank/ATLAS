db8c4c907b65990a327fbf1b037f9639fa72a9f6
Hannes Frank <hannes.frank@online.de>
2016-11-28 14:31:17 +0100

    Added test for dcAddLogEntry().

--- src/test.cpp
+++ src/test.cpp
@@ -6,13 +6,13 @@
 extern "C"
 {
 #include "../inc/Date.h"
+#include "../inc/DayCounter.h"
 #include "../inc/LogEntry.h"
 #include "../inc/parse.h"
 extern unsigned short toHttpMethod(const char *str);
 extern unsigned char toMonth(const char *str);
 };
 
-// TODO: test lgeAddToDaycounter()
 // TODO: test struct LogStats
 
 // TODO: more tests for parseLogEntry()
@@ -80,8 +80,41 @@ TEST_CASE("date to string", "[src/Date.c/dtToString]")
 }
 
 //
-// ============   LogEntry.h   ==========
+// ============   DayCounter.h   ==========
 //
+TEST_CASE("day counter add log entry", "[src/DayCounter.c/dcAddLogEntry]")
+{
+	struct DayCounter counter;
+	struct LogEntry entry0;
+	struct LogEntry entry1;
+	entry0.remote_address = (char*)"127.0.0.1";
+	entry0.request_size = 420;
+	entry0.response_size = 1337;
+	entry1.remote_address = (char*)"example.com";
+	entry1.request_size = 12;
+	entry1.response_size = 100;
+
+	memset(&counter, 0, sizeof(struct DayCounter));
+
+	REQUIRE_NOTHROW(dcAddLogEntry(NULL, NULL));
+	REQUIRE_NOTHROW(dcAddLogEntry(NULL, &entry0));
+	REQUIRE_NOTHROW(dcAddLogEntry(&counter, NULL));
+
+	dcAddLogEntry(&counter, &entry0);
+
+	REQUIRE(counter.n_requests == 1);
+	// REQUIRE(counter.n_users == 1);
+	REQUIRE(counter.n_bytes_in == 420);
+	REQUIRE(counter.n_bytes_out == 1337);
+
+	dcAddLogEntry(&counter, &entry1);
+	dcAddLogEntry(&counter, &entry0);
+
+	REQUIRE(counter.n_requests == 3);
+	// REQUIRE(counter.n_users == 2);
+	REQUIRE(counter.n_bytes_in == 852);
+	REQUIRE(counter.n_bytes_out == 2774);
+}
 
 //
 // ============   parse.h   ==========
