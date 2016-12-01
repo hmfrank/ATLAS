c7084adcfce4f269040b5f0f962ee89f32f60196
Hannes Frank <hannes.frank@online.de>
2016-12-01 09:29:10 +0100

    Added unit test for AvlTree.

--- tst/AvlTree.cpp
+++ tst/AvlTree.cpp
@@ -5,4 +5,37 @@ extern "C"
 #include "../inc/AvlTree.h"
 }
 
-// TODO: test avl tree
+int compare(const void *a, const void *b)
+{
+	return (int)a - (int)b;
+}
+
+TEST_CASE("avl tree contains, delete, insert, is empty", "[inc/AvlTree.h/avlContains, inc/AvlTree.h/avlDelete, inc/AvlTree.h/avlInsert, inc/AvlTree.h/avlIsEmpty]")
+{
+	struct AvlTree tree;
+
+	avlReset(&tree, &compare);
+	REQUIRE(avlIsEmpty(&tree));
+
+	for (int i = 0; i < 10; i++)
+		REQUIRE(avlInsert(&tree, (void*)i));
+
+	for (int i = 0; i < 10; i++)
+		REQUIRE(avlContains(&tree, (void*)i));
+
+	REQUIRE_FALSE(avlIsEmpty(&tree));
+	REQUIRE_FALSE(avlInsert(&tree, (void*)2));
+	REQUIRE_FALSE(avlContains(&tree, (void*)-1));
+	REQUIRE_FALSE(avlDelete(&tree, (void*)-2));
+
+	for (int i = 0; i < 10; i++)
+		REQUIRE(avlDelete(&tree, (void*)i));
+
+	REQUIRE(avlIsEmpty(&tree));
+
+	for (int i = 0; i > -10; i--)
+		avlInsert(&tree, (void*)i);
+
+	avlReset(&tree, NULL);
+	REQUIRE(avlIsEmpty(&tree));
+}
